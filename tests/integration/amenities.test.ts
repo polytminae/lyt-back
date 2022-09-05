56a85febef74f7894360a8a5447f7cf3ef564b36
gralmeidan <gralmeidan@gmail.com>
2022-09-05 09:57:30 -0400

    Implemented tests for newly added routes

--- tests/integration/amenities.test.ts
+++ tests/integration/amenities.test.ts
@@ -11,6 +11,31 @@ afterEach(() => {
 });
 
 describe('Testa o método GET em /amenities', () => {
+  describe('Testa a rota /amenities', () => {
+    const query = `
+      SELECT am.id,
+      am.name,
+      cat.name AS category
+    FROM amenities AS am
+      INNER JOIN categories AS cat ON am.category_id = cat.id;`;
+
+    it('Deve retornar todos os serviços registrados', async () => {
+      const stub = sinon
+        .stub(connection, 'execute')
+        .resolves(require('../mocks/amenities/fullSQLResponse.json'));
+
+      const response = await chai.request(app).get('/amenities');
+
+      expect(stub.calledOnce).to.be.true;
+      expect(stub.firstCall.calledWith(sinon.match(query)));
+
+      expect(response.status).to.equal(200);
+      expect(response.body).to.deep.equal(
+        require('../mocks/amenities/fullResult')
+      );
+    });
+  });
+
   describe('Testa a rota /amenities/rental/:id', () => {
     it('Deve retornar todos os serviços de um apartamento', async () => {
       sinon
