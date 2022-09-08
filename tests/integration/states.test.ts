77183892e6f4a02c313f57927f0d117ba6da0622
gralmeidan <gralmeidan@gmail.com>
2022-09-08 09:46:25 -0400

    Implemented tests for newly added routes

--- tests/integration/states.test.ts
+++ tests/integration/states.test.ts
@@ -3,7 +3,7 @@ import chaiHttp from 'chai-http';
 import sinon from 'sinon';
 import app from '../../src/app';
 import connection from '../../src/models/connection';
-import { modelOutput, SQLResponse } from '../mocks/cities.model.mock';
+import { modelOutput, SQLResponse } from '../mocks/states.model.mock';
 
 chai.use(chaiHttp);
 
@@ -11,15 +11,15 @@ afterEach(sinon.restore);
 
 describe('Testa o método GET em /cities', () => {
   describe('Testa a rota /cities', () => {
-    it('Deve retornar todas as cidades registradas', async () => {
+    it('Deve retornar todos os estados registrados', async () => {
       const stub = sinon.stub(connection, 'execute').resolves(SQLResponse);
 
-      const response = await chai.request(app).get('/cities');
+      const response = await chai.request(app).get('/states');
       const [query] = stub.firstCall.args;
 
       expect(stub.calledOnce).to.be.true;
       expect(query).not.to.include('WHERE');
-      expect(query).to.include('FROM cities');
+      expect(query).to.include('FROM states');
       expect(response.status).to.equal(200);
       expect(response.body).to.deep.equal(modelOutput);
     });
---
+++ tests/mocks/states.model.mock.ts
@@ -0,0 +1,47 @@
+export const SQLResponse: any = [
+  [
+    {
+      id: 1,
+      name: 'São Paulo',
+      short: 'SP',
+    },
+    {
+      id: 2,
+      name: 'Rio Grande do Sul',
+      short: 'RS',
+    },
+    {
+      id: 3,
+      name: 'Rio de Janeiro',
+      short: 'RJ',
+    },
+    {
+      id: 4,
+      name: 'Minas Gerais',
+      short: 'MG',
+    },
+  ],
+];
+
+export const modelOutput = [
+  {
+    id: 1,
+    name: 'São Paulo',
+    short: 'SP',
+  },
+  {
+    id: 2,
+    name: 'Rio Grande do Sul',
+    short: 'RS',
+  },
+  {
+    id: 3,
+    name: 'Rio de Janeiro',
+    short: 'RJ',
+  },
+  {
+    id: 4,
+    name: 'Minas Gerais',
+    short: 'MG',
+  },
+];
